4a8b306e396fcfeb78c0c29c2e347db15f3b0748
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-28 18:09:55 -0300

    persistir Objeto con JPA en Java EE

--- Leccio4/sga-jee-web/src/main/java/arg/com/gm/sga/cliente/ciclovidajpa/PersistirObjetoJPA.java
+++ Leccio4/sga-jee-web/src/main/java/arg/com/gm/sga/cliente/ciclovidajpa/PersistirObjetoJPA.java
@@ -26,6 +26,24 @@ public class PersistirObjetoJPA {
         //paso 1. Crea nuevo objeto
         //Objeto en estado transitivo(no esta ligado con la base de datos ya que
         // esta transaccion todavia no inicio)
-        Persona persona1 = new Persona("erica", "condorii", "econdori@mail.com", "165165165");
+        Persona persona1 = new Persona("ericaa", "condorii", "econdori@mail.com", "165165165");
+        //paso 2. Iniciar la transaccion
+        txt.begin();
+
+        //paso 3.Ejecutamos SQL(todavia no se inserta en la base de datos ni se
+        // genera la llave primaria)
+        em.persist(persona1);
+        log.debug("Objeto persistido estado dtached: "+persona1);
+
+        //paso 4.commit o rollback(cuando hacemos commit se guarda el 
+        //objeto en la base de datos y se genera la pk)
+        txt.commit();
+        
+        //objeto en estado detached 
+        log.debug("Objeto persistido estado dtached: "+persona1);
+        
+        //cerramos el entytimanager
+        em.close();
+
     }
 }
