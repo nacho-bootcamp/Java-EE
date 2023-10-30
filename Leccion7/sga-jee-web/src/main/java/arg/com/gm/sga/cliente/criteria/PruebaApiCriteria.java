f527b08abc56ec8bbf7ec0285d88ef0b7bdd2cdd
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-29 21:51:45 -0300

    Consulta con el API de criteria  con JPA en Java EE part2

--- Leccion7/sga-jee-web/src/main/java/arg/com/gm/sga/cliente/criteria/PruebaApiCriteria.java
+++ Leccion7/sga-jee-web/src/main/java/arg/com/gm/sga/cliente/criteria/PruebaApiCriteria.java
@@ -5,7 +5,8 @@
 package arg.com.gm.sga.cliente.criteria;
 
 import arg.com.gm.sga.domain.Persona;
-import java.util.List;
+import java.util.*;
+
 import javax.persistence.*;
 import javax.persistence.criteria.*;
 import org.apache.logging.log4j.*;
@@ -27,36 +28,75 @@ public class PruebaApiCriteria {
         Root<Persona> fromPersona = null;
         TypedQuery<Persona> query = null;
         Persona persona = null;
-        List<Persona>personas=null;
-        
+        List<Persona> personas = null;
+
         //query utilizando el API de Criteria 
         //1.Consulta de todas las personas
-        
         //Paso1. El objeto EntityManager crea una instancia CriteriaBuilder
         cb = em.getCriteriaBuilder();
-        
+
         //Paso2. Se crea un objeto CriteriaQuery
-        criteriaQuery=cb.createQuery(Persona.class);
-        
+        criteriaQuery = cb.createQuery(Persona.class);
+
         //Paso3. Creamos el objeto raiz de query
-        fromPersona=criteriaQuery.from(Persona.class);
-        
+        fromPersona = criteriaQuery.from(Persona.class);
+
         //Paso4 seleccionamos lo necesario del from
         criteriaQuery.select(fromPersona);
-        
+
         //Paso5 Creamo el query typeSafe
-        query=em.createQuery(criteriaQuery);
-        
+        query = em.createQuery(criteriaQuery);
+
         //Paso6 Ejecutamos la consulta
-       personas=query.getResultList();
+        personas = query.getResultList();
+
+        //mostrarPersonas(personas);
+        //2-a. Consulta de la Persona con id=1;
+        log.debug("\n2-a Consulta de la persona con id=1");
+        cb = em.getCriteriaBuilder();
+        criteriaQuery = cb.createQuery(Persona.class);
+        fromPersona = criteriaQuery.from(Persona.class);
+        criteriaQuery.select(fromPersona).where(cb.equal(fromPersona.get("idpersona"), 1));
+        persona = em.createQuery(criteriaQuery).getSingleResult();
+        // log.debug(persona);
+
+        //2-b. Consulta de la Persona con el id=1
+        log.debug("\n2-b Consulta de la Persona con el id=1");
+        cb = em.getCriteriaBuilder();
+        criteriaQuery = cb.createQuery(Persona.class);
+        fromPersona = criteriaQuery.from(Persona.class);
+        criteriaQuery.select(fromPersona);
+
+        //La clase  Predicate permite agregar varios criterios dinamicamente
+        List<Predicate> criterios = new ArrayList<Predicate>();
+
+        //Verificamos si tenemos criterios que agregar
+        Integer idPersonaParam = 1;
+        ParameterExpression<Integer> parameter = cb.parameter(Integer.class, "idpersona");
+        criterios.add(cb.equal(fromPersona.get("idpersona"), parameter));
+
+        //se agregan los criterios
+        if (criterios.isEmpty()) {
+            throw new RuntimeException("Sin criterios");
+        } else if (criterios.size() == 1) {
+            criteriaQuery.where(criterios.get(0));
+        }else{
+        criteriaQuery.where(cb.and(criterios.toArray(new Predicate[0])));
+        }
         
-        mostrarPersonas(personas);
+        query=em.createQuery(criteriaQuery);
+        query.setParameter("idpersona", idPersonaParam);
         
-        
+        //se ejecuta el query
+        persona =query.getSingleResult();
+        log.debug(persona);
+
     }
 
     private static void mostrarPersonas(List<Persona> personas) {
-    for(Persona p : personas)
-        log.debug(p);
-    };
+        for (Persona p : personas) {
+            log.debug(p);
+        }
+    }
+;
 }
