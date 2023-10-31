855f3bba8d3aa4caa127ae6a7b934bbc8bc8a549
ignacio leonel cardozo <ignaciocardozoxd12@gmail.com>
2023-10-30 23:22:51 -0300

    aplicacion web con jsf mas primefaces part2

---
+++ Leccion9/sga-jee-web/src/main/java/arg/com/gm/sga/web/PersonaBean.java
@@ -0,0 +1,59 @@
+package arg.com.gm.sga.web;
+
+import arg.com.gm.sga.domain.Persona;
+import arg.com.gm.sga.servicio.PersonaService;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import org.apache.logging.log4j.*;
+import org.primefaces.event.RowEditEvent;
+
+@Named("personaBean")
+@RequestScoped
+public class PersonaBean {
+
+Logger log = LogManager.getRootLogger();
+
+    @Inject
+    private PersonaService personaService;
+
+    private Persona personaSeleccionada;
+
+    List<Persona> personas;
+
+    public PersonaBean() {
+        log.debug("Iniciando el objeto persona Beans");
+    }
+
+    @PostConstruct
+    public void inicializar() {
+        //iniciamos las variables
+        this.personas = personaService.listarPersona();
+        log.debug("Personas recuperadas en ManagedBean "+this.personas);
+        this.personaSeleccionada = new Persona();
+    }
+    
+    public void editListener(RowEditEvent event){
+    Persona persona=(Persona) event.getObject();
+    personaService.modificarPersona(persona);
+    }
+    
+        public Persona getPersonaSeleccionada() {
+        return personaSeleccionada;
+    }
+
+    public void setPersonaSeleccionada(Persona personaSeleccionada) {
+        this.personaSeleccionada = personaSeleccionada;
+    }
+
+    public List<Persona> getPersonas() {
+        return personas;
+    }
+
+    public void setPersonas(List<Persona> personas) {
+        this.personas = personas;
+    }
+
+}
